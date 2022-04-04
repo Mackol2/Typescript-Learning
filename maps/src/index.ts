55fd94289d4602d41a58e74d32c9decb65894ecc
user <user@MacBook-Pro-admin.local>
2022-04-04 11:56:41 +0200

    Załadowanie projektu

---
+++ maps/src/index.ts
@@ -0,0 +1,12 @@
+import {User} from "./User";
+import {Company} from "./Companies";
+
+
+new google.maps.Map(document.getElementById('map'), {
+        zoom: 1,
+        center: {
+            lat: 0,
+            lng: 0
+        }
+    }
+);
